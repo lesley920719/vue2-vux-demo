9c728344190cb7333f161dab337ab569e9d84618
lijian <li.jian@vpclub.cn>
2019-04-01 16:32:00 +0800

    proxyTable代理配置

--- config/index.js
+++ config/index.js
@@ -25,9 +25,23 @@ module.exports = {
     env: require('./dev.env'),
     port: 8099,
     autoOpenBrowser: false,
+    // 静态资源文件夹
     assetsSubDirectory: 'static',
+    // 发布路径
     assetsPublicPath: '/',
-    proxyTable: {},
+    // 代理配置表，在这里可以配置特定的请求代理到对应的API接口
+    // 例如将'localhost:8099/api/xxx'代理到'www.example.com/api/xxx'
+    // 使用方法：https://vuejs-templates.github.io/webpack/proxy.html
+    proxyTable: {
+      '/api': { // '/api': 配置项 -- 这里理解成 /api 代替target的地址，调接口时直接用 /api 代替，比如调用'https://c.y.qq.com/user/add'，直接写 '/api/user/add'即可
+        target: 'http://xxxxxx.com',//设置调用的接口域名（跨域地址）
+        changeOrigin: true, // 是否跨域
+        secure: false,  // 是否使用https
+        pathRewrite: {  // 重写地址：如果接口本身没有/api这种通用前缀，需要通过pathRewrite来重写地址
+          '^/api': "",
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
